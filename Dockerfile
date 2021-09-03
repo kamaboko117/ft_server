03828645e66b309a2da6074b6ff53e29d7cecc0f
Alexander Sabouret <alexandersabouret@gmail.com>
2021-09-03 18:44:17 +0200

    too much to even list: can now open a website index at localhost with test.php

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
 FROM debian:buster
 
+RUN apt-get update
+RUN apt-get install nginx -y
+RUN apt-get install wget -y
+RUN apt-get install openssl
+RUN apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline
+RUN apt-get install -y mariadb-server mariadb-client
+RUN apt-get install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
+
 COPY srcs/service_start.sh .
+COPY srcs/test.php .
+COPY srcs/default .
+COPY srcs/config.inc.php .
+COPY srcs/wp.config.php .
 
 CMD bash service_start.sh
--- srcs/service_start.sh
+++ srcs/service_start.sh
@@ -1,18 +1,33 @@
-apt-get update
-apt-get install nginx -y
-
-service nginx start
-apt-get install openssl
+mkdir /var/www/localhost
+mkdir /var/www/localhost/wordpress
 mkdir /etc/nginx/ssl
+mv default /etc/nginx/sites-available/
+mv test.php /var/www/localhost
+mv ./wp-config.php /var/www/localhost/wordpress
+
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
 	-out /etc/nginx/ssl/localhost.pem \
 	-keyout /etc/nginx/ssl/localhost.key \
 	-subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=kama/CN=localhost"
 
-apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli \
-	php7.3-common php7.3-json php7.3-opcache php7.3-readline
+wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-languages.tar.gz
+tar -zxvf phpMyAdmin-5.1.1-all-languages.tar.gz
+mv phpMyAdmin-5.1.1-all-languages /var/www/localhost/phpMyAdmin
+mv config.inc.php /var/www/localhost/phpMyAdmin/
+
+service mysql start
+echo "GRANT ALL ON *.* TO 'kama'@'localhost' IDENTIFIED BY '123'" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
+
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+cp -a wordpress/. /var/www/localhost/wordpress
+
+service mysql restart
 service php7.3-fpm start
-mkdir /var/www/localhost
+service nginx start
 
 sleep infinity
 #apt-get install mariadb-server
